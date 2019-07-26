005506888515fe38dcde12bb9ad5a8b226d4b701
Levi Strange <levi.strange@clickon.co>
2019-07-26 17:18:11 +0100

    Updates

---
+++ precache-manifest.bbbf7837d90bd95995e292df1d74b186.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4b0401de6de68f68e9f1c6e88fd9b8",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "f4c3092467886490f3ae",
+    "url": "/react-pokedex/static/css/main.4cbe6066.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-pokedex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f4c3092467886490f3ae",
+    "url": "/react-pokedex/static/js/main.2cf0496a.chunk.js"
+  },
+  {
+    "revision": "49325ecd73a50f0ca547",
+    "url": "/react-pokedex/static/js/runtime~main.0848e284.js"
+  }
+]);
